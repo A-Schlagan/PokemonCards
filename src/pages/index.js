69bcd0a8d2cf4460302b3951570a29646c06d497
A-Schlagan <Alexander.schlagan@gmail.com>
2026-01-29 12:35:16 +0100

    upd Suchleiste erstellt

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,38 +17,61 @@ class Index extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      searchTerm: ""
+    };
   }
 
-  componentWillMount() {
-
-
-  }
-
-  componentDidMount() {
-
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-
+  handleSearchChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
   }
 
 
-  render() {
-    return (
-      <div>
-        <h1>Pokedex</h1>
-        <div style={{ display: "flex", gap: "20px", flexWrap: "wrap" }}>
-          {this.pokemons.map((pokemon) => {
-            return <PokemonCard name={pokemon.name} type={pokemon.type} />
-          })}
-        </div></div>
-    );
+
+    componentWillMount() {
+
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+    }
+
+
+    render() {
+      const filteredPokemons = this.pokemons.filter(pokemon =>
+        pokemon.name.toLowerCase().includes(this.state.searchTerm.toLowerCase()) ||
+        pokemon.type.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+      );
+
+      return (
+        <div>
+          <h1>Pokedex</h1>
+
+          <input
+            type="text"
+            placeholder="Suche nach Name oder Typ..."
+            value={this.state.searchTerm}
+            onChange={this.handleSearchChange}
+            style={{ padding: "10px", width: "250px", marginBottom: "50px" }}
+          />
+
+          <div style={{ display: "flex", gap: "20px", flexWrap: "wrap" }}>
+            {filteredPokemons.map((pokemon) => {
+              return <PokemonCard name={pokemon.name} type={pokemon.type} />
+            })}
+          </div></div>
+      );
+    }
   }
-}
 
 
 export default Index;
